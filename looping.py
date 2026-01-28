e50524570ec6f23009538530d7ca3b4345404076
kkandkur-mech <kkandkur@student.gitam.edu>
2026-01-28 11:15:44 +0530

    Create looping.py

---
+++ looping.py
@@ -0,0 +1,25 @@
+print("For Loop:")
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    print(fruit)
+
+print()  
+
+print("For Loop with Range:")
+for i in range(5): 
+    print(i)
+
+print() 
+
+print("While Loop:")
+count = 0
+while count < 5:
+    print(count)
+    count += 1 
+
+print()  
+
+print("Emulating Do-While Loop:")
+while True:
+    print("This will always run at least once.")
+    break  
