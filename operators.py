74dd672bd2b3e4e4265d046916b64ac19a557da6
kkandkur-mech <kkandkur@student.gitam.edu>
2026-01-29 15:00:19 +0530

    Create operators.py
    IN this code we performed different operations in anthemic and given output

---
+++ operators.py
@@ -0,0 +1,31 @@
+a = 10
+b = 3
+
+addition = a + b
+print(f"Addition: {a} + {b} = {addition}")
+
+subtraction = a - b
+print(f"Subtraction: {a} - {b} = {subtraction}")
+
+multiplication = a * b
+print(f"Multiplication: {a} * {b} = {multiplication}")
+
+division = a / b
+print(f"Division: {a} / {b} = {division}")
+
+floor_division = a // b
+print(f"Floor Division: {a} // {b} = {floor_division}")
+
+modulus = a % b
+print(f"Modulus: {a} % {b} = {modulus}")
+exponentiation = a ** b
+print(f"Exponentiation: {a} ** {b} = {exponentiation}")
+#OUTPUT
+Addition: 10 + 3 = 13
+Subtraction: 10 - 3 = 7
+Multiplication: 10 * 3 = 30
+Division: 10 / 3 = 3.3333333333333335
+Floor Division: 10 // 3 = 3
+Modulus: 10 % 3 = 1
+Exponentiation: 10 ** 3 = 1000
+
