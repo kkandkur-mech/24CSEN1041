1d7626c6e4b233f03853c6ad96a9d46f70a3ef59
kkandkur-mech <kkandkur@student.gitam.edu>
2026-01-29 15:39:12 +0530

    Create logical operators.py

---
+++ logical operators.py
@@ -0,0 +1,24 @@
+# Defining Boolean variables
+a = True
+b = False
+
+# Using 'and' operator
+result_and = a and b  # True only if both a and b are True
+
+# Using 'or' operator
+result_or = a or b  # True if at least one of a or b is True
+
+# Using 'not' operator
+result_not_a = not a  # Inverts the value of a
+result_not_b = not b  # Inverts the value of b
+
+# Outputting results
+print("a and b:", result_and)       # Output: False
+print("a or b:", result_or)         # Output: True
+print("not a:", result_not_a)       # Output: False
+print("not b:", result_not_b)       # Output: True
+#output
+a and b: False
+a or b: True
+not a: False
+not b: True
