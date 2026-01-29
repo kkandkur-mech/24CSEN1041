baa167bcc999e09c56e84390f9eee6e7b558f8a8
kkandkur-mech <kkandkur@student.gitam.edu>
2026-01-29 15:37:04 +0530

    Create Comparison operators.py

---
+++ Comparison operators.py
@@ -0,0 +1,20 @@
+# Comparison operators in Python
+
+a = 10
+b = 20
+c = 10
+
+# Using comparison operators
+print("a == b:", a == b)          # False
+print("a != b:", a != b)          # True
+print("a > b:", a > b)            # False
+print("a < b:", a < b)            # True
+print("a >= c:", a >= c)          # True
+print("b <= c:", b <= c)          # False
+#output
+a == b: False
+a != b: True
+a > b: False
+a < b: True
+a >= c: True
+b <= c: False
